c5592186818cde90aff95bcbd50384c6cbcd70b0
Ivan Lerner <ivanlerner47@gmail.com>
2023-04-30 17:02:27 +0300

    add fifth lab

---
+++ src/main/java/ua/lviv/iot/algo/part1/lab5/Main.java
@@ -0,0 +1,7 @@
+package ua.lviv.iot.algo.part1.lab5;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/ua/lviv/iot/algo/part1/lab5/TvChanger.java
@@ -0,0 +1,7 @@
+package ua.lviv.iot.algo.part1.lab5;
+
+import java.util.regex.Pattern;
+
+public class TvChanger {
+    private static final Pattern TV_PATTERN = Pattern.compile("");
+}
